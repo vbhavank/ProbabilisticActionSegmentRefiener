6d0332af26eb2052254a3168689aa63859fe551b
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2024-03-16 12:35:32 -0700

    uncertainty measurement for each action segment 1-22

--- main.py
+++ main.py
@@ -502,4 +502,4 @@ if __name__ == '__main__':
     print(most_uncertain_segments)
     result_dict, _ = trainer.test(test_test_dataset, mode="decoder-agg", device='cuda', label_dir=label_dir, result_dir=f"{result_dir}/{naming}", model_path=model_path, most_uncertain_segments=most_uncertain_segments)
     with open(f"{result_matrices}/with_mask_metrices.json", "w") as outfile: 
-        json.dump(result_dict, outfile)
+        json.dump(result_dict, outfile, cls=NumpyFloatEncoder)
