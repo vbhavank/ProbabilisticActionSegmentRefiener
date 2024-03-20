e9f1fd2b1714f9127899bfcbbe803a07c774c433
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2024-03-20 12:27:07 -0700

    PGM uncertain masking 1-97

--- main.py
+++ main.py
@@ -736,7 +736,7 @@ if __name__ == '__main__':
     print(f"PGM most uncertain mask:")
     video_most_uncertain_segment_map = get_most_uncertain_segment_PGM(naming, label_dir, f"{result_dir}/{naming}/prediction_print", trainer, test_test_dataset, model_path, device='cuda')
  
-    # result_dict, most_uncertain_segments, mistaken_frames, random_frames = trainer.test(test_test_dataset, mode="decoder-agg", device='cuda', label_dir=label_dir, result_dir=f"{result_dir}/most_uncertain_segment_PGM/{naming}", model_path=model_path, video_most_uncertain_segment_map=video_most_uncertain_segment_map)
+    result_dict, most_uncertain_segments, mistaken_frames, random_frames = trainer.test(test_test_dataset, mode="decoder-agg", device='cuda', label_dir=label_dir, result_dir=f"{result_dir}/most_uncertain_segment_PGM/{naming}", model_path=model_path, video_most_uncertain_segment_map=video_most_uncertain_segment_map)
     # with open(f"{result_matrices}/without_mask_metrices.json", "w") as outfile: 
     #     json.dump(result_dict, outfile, cls=NumpyFloatEncoder)
 
@@ -751,8 +751,8 @@ if __name__ == '__main__':
     # with open(f"{result_matrices}/with_mismatch_mask_metrices.json", "w") as outfile: 
     #     json.dump(result_dict, outfile, cls=NumpyFloatEncoder)
 
-    print(f"\nWith random mask:")
-    result_dict, _, _, _ = trainer.test(test_test_dataset, mode="decoder-agg", device='cuda', label_dir=label_dir, result_dir=f"{result_dir}/random/{naming}", model_path=model_path, random_mask=random_frames)
+    # print(f"\nWith random mask:")
+    # result_dict, _, _, _ = trainer.test(test_test_dataset, mode="decoder-agg", device='cuda', label_dir=label_dir, result_dir=f"{result_dir}/random/{naming}", model_path=model_path, random_mask=random_frames)
     # with open(f"{result_matrices}/with_random_mask_metrices.json", "w") as outfile: 
     #     json.dump(result_dict, outfile, cls=NumpyFloatEncoder)
 
